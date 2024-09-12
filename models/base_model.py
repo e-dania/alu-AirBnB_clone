af6d8393289da1a9292f14eb20e9fd8241057d4f
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-09-12 14:08:25 +0200

    here we go

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
-mport datetime
+import datetime
 import uuid
-rom engine.file_storage import FileStorage
+from engine.file_storage import FileStorage
 class BaseModel:
-   
-   """Id(int), created_at, updated_at.Base Line Attributes."""
-   def __init__(self, *args, **kwargs):
-     """States all Class attributes and initialises them"""
-es = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
-            for spec, value in kwargs.items():
-                if spec == "__class__":
-                    continue
-                elif spec == "created_at" or spec == "updated_at":
-                    setattr(self, spec, datetime.strptime(value, timePres))
-                else:
-                    setattr(self, spec, value)
-        else:
-            """id special here. uses uuid4 and converted to string"""
+
+    """Id(int), created_at, updated_at.Base Line Attributes."""
+	def __init__(self, *args, **kwargs):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = self.created_at
@@ -44,25 +32,3 @@ es = "%Y-%m-%dT%H:%M:%S.%f"
             data_to_dict[key] = value
 
         return data_to_dict
-    
-if __name__ == "__main__":
-    """Creating BaseModel Object then converts it into dictionary
-    Once dictionary it is waits for potential serialization"""
-    base_model = BaseModel();
-    base_model.number = 12
-    base_model.name = "FIRST DATA MODEL HERE"
-    print(base_model)
-    base_model.save()
-    print(base_model)
-    base_model_json = base_model.to_dict()
-    print(base_model_json)
-    print("JSON data version of original base model dude:")
-    for spec in base_model_json.keys():
-        print("\t{}: ({}) - {}".format(spec, type(base_model_json[spec]), base_model_json[spec]))
-    else:
-        print("None")
-
-    
-
-
-

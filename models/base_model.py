4dd4dc686bb938a5c266f898d523e06bce1f5d73
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-09-12 14:33:45 +0200

    errors for no reason

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""This Class file is the base clase for literally all core features"""
 import datetime
 import uuid
 from engine.file_storage import FileStorage
 class BaseModel:
+    """Base Model Class."""
 
-    """Id(int), created_at, updated_at.Base Line Attributes."""
-	def __init__(self, *args, **kwargs):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = self.created_at
-            self.storage = FileStorage()
-
+    def __init__(self, *args, **kwargs):
+        """Id(int), created_at, updated_at.Base Line Attributes."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = self.created_at
+        self.storage = FileStorage()
 
     def __str__(self):
+
         """Returns user objject in string format"""
         classname = f"[<{self.__class.__name__}>({self.id})] {self.__dict__}"
         return classname
@@ -30,5 +32,4 @@ class BaseModel:
             if key == 'created_at' or key == 'updated_at':
                 value = value.isoformat()
             data_to_dict[key] = value
-
         return data_to_dict

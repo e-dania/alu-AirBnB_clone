551575a190c22bf49c6c40ba1bb524782ad30df6
Sipho <62767262+Siphocha@users.noreply.github.com>
2024-09-14 01:37:00 +0200

    INIT FILE I FORGOT

---
+++ models/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""File to initialise all classes and
+global spanning variables"""
+
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+"""Mention storage as this where all class data saved to."""
+storage = FileStorage()
+storage.reload()
+
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
+}

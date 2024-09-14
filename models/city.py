b9ad66527319a4d028a4fd42aad8b86e9be88513
e-dania <156061632+e-dania@users.noreply.github.com>
2024-09-14 19:52:19 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -8,7 +8,3 @@ class City(BaseModel):
     """Class for the city data"""
     state_id = ""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialsing empty arguments because initialisation needed"""
-        super().__init(*args, **kwargs)

feae93a0fe2322915cd1e35e8c3387708bd20fb0
e-dania <156061632+e-dania@users.noreply.github.com>
2024-09-14 19:48:37 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-
 """Class for holding user "amenities" data"""
-
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     """Class to hold amenities related user data"""
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialsing empty arguments because initialisation needed"""
-        super().__init(*args, **kwargs)

cdcb5ea0959148d122016a3068043c7a553280f2
e-dania <156061632+e-dania@users.noreply.github.com>
2024-09-14 19:52:59 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -16,7 +16,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
-    def __init__(self, *args, **kwargs):
-        """Initialsing empty arguments because initialisation needed"""
-        super().__init(*args, **kwargs)

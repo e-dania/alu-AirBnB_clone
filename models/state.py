958cec68d892410e690ca5d0d26d775c4c08738f
e-dania <156061632+e-dania@users.noreply.github.com>
2024-09-14 19:54:06 +0200

    Update state.pyinitializationnot needed

--- models/state.py
+++ models/state.py
@@ -6,6 +6,3 @@ from models.base_model import BaseModel
 class State(BaseModel):
     name = ""
 
-    def __init__(self, *args, **kwargs):
-        """Initialsing empty arguments because initialisation needed"""
-        super().__init(*args, **kwargs)

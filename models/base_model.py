c94dcbc550db8fd72bdf9cbab9ecbee4ed94bee9
e-dania <156061632+e-dania@users.noreply.github.com>
2024-09-14 19:51:25 +0200

    sighhhh

--- models/base_model.py
+++ models/base_model.py
@@ -34,3 +34,29 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
+def __str__(self):
+        """
+        Returns a string representation of the instance in the format:
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the `updated_at` attribute with the current datetime and
+        saves the instance to storage.
+        """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the instance.
+        Converts datetime fields to ISO format and adds the class name.
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
